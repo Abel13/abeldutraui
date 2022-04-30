14b7d7e511ddc02a1d0436ec838d13ea8e3fc973
Abel <abel.o.d@outlook.com>
2022-04-30 20:49:09 -0300

    feat: added lottie

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Lottie from "react-lottie";
+import animationData from "./lotties/website-under-construction.json";
+import "./App.css";
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Lottie options={defaultOptions} />
+        </div>
       </header>
     </div>
   );
