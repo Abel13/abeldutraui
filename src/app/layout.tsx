83ab75e6e1d449a9531f2b5aefcfee903bc9e0e6
Abel Dutra <abel.o.d@outlook.com>
2025-05-05 17:49:10 -0300

    fix: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
+import { inter, poppins } from "./fonts";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
-
-export const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
-
-export const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["600", "700", "800"],
-  variable: "--font-poppins",
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Abel Dutra UI",
