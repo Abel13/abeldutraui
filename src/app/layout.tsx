6f15349249ec770c00eece5a1c67474fe30b9c7d
Abel Dutra <abel.o.d@outlook.com>
2025-05-07 15:13:28 -0300

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { inter, poppins } from "./fonts";
 import "./globals.css";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Abel Dutra UI",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${poppins.variable} antialiased`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
